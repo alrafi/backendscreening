a70ac44044a961a1e1b93baf4e92bd9d5bacd08c
alrafi <hafisalrafi@gmail.com>
2020-10-15 16:47:41 +0700

    add model user

--- model/model.go
+++ model/model.go
@@ -1,5 +1,14 @@
 package model
 
+// User is model of user
+type User struct {
+	Username string `json:"username"`
+	Fullname string `json:"fullname"`
+	Birthday string `json:"birthday"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 // Diary is model of diary
 type Diary struct {
 	ID      string `json:"id"`
@@ -13,4 +22,11 @@ type Response struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
 	Data    []Diary
+}
+
+// ResponseRegister is model of response result
+type ResponseRegister struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []User
 }
